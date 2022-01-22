87456122344d718e8297261c9568a405eea626af
adrg2 <adr.garro@gmail.com>
2022-01-22 19:00:27 +0100

    Removed magic numbers and encapsulate item logic

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,28 +1,31 @@
 package com.gildedrose
 
+private const val QUALITY_MAX = 50
+private const val QUALITY_MIN = 0
+
 class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (!items[i].isAgedBrie && !items[i].isBackstage) {
+                if (items[i].quality > QUALITY_MIN) {
+                    if (!items[i].isSulfuras) {
                         items[i].quality = items[i].quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (items[i].quality < QUALITY_MAX) {
                     items[i].quality = items[i].quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (items[i].isBackstage) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < QUALITY_MAX) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < QUALITY_MAX) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
@@ -30,15 +33,15 @@ class GildedRose(var items: List<Item>) {
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (!items[i].isSulfuras) {
                 items[i].sellIn = items[i].sellIn - 1
             }
 
             if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                if (!items[i].isAgedBrie) {
+                    if (!items[i].isBackstage) {
+                        if (items[i].quality > QUALITY_MIN) {
+                            if (!items[i].isSulfuras) {
                                 items[i].quality = items[i].quality - 1
                             }
                         }
@@ -46,7 +49,7 @@ class GildedRose(var items: List<Item>) {
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (items[i].quality < QUALITY_MAX) {
                         items[i].quality = items[i].quality + 1
                     }
                 }
--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -1,3 +1,17 @@
 package com.gildedrose
 
 data class Item(var name: String, var sellIn: Int, var quality: Int)
+
+val Item.isAgedBrie: Boolean
+    get() = name == "Aged Brie"
+
+val Item.isBackstage: Boolean
+    get() = name == "Backstage passes to a TAFKAL80ETC concert"
+
+val Item.isSulfuras: Boolean
+    get() = name == "Sulfuras, Hand of Ragnaros"
+
+fun Item.incrementQuality() = quality + 1
+fun Item.decrementQuality() = quality - 1
+fun Item.incrementSellIn() = sellIn + 1
+fun Item.decrementSellIn() = sellIn - 1
