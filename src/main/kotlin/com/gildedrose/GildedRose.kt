d2553ba1867d5b750bda8449ae83756dc74b4f10
adrg2 <adr.garro@gmail.com>
2022-01-23 22:39:02 +0100

    Removed inverse sulfuras condition

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,6 +5,7 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
+            if (items[i].isSulfuras) continue
             if (items[i].isAgedBrie || items[i].isBackstage) {
                 items[i].quality = items[i].incrementQuality()
 
@@ -19,33 +20,28 @@ class GildedRose(var items: List<Item>) {
                 }
             } else {
                 if (items[i].quality > QUALITY_MIN) {
-                    if (!items[i].isSulfuras) {
-                        if (items[i].isConjured) {
-                            items[i].quality = items[i].decrementQuality(2)
-                        } else {
-                            items[i].quality = items[i].decrementQuality()
-                        }
+                    if (items[i].isConjured) {
+                        items[i].quality = items[i].decrementQuality(2)
+                    } else {
+                        items[i].quality = items[i].decrementQuality()
                     }
                 }
             }
 
-            if (!items[i].isSulfuras) {
-                // normales y conjured
-                items[i].sellIn = items[i].decrementSellIn()
-            }
+            // normales y conjured
+            items[i].sellIn = items[i].decrementSellIn()
+
 
             if (items[i].isSellInExpired()) {
                 if (items[i].isAgedBrie) {
                     items[i].quality = items[i].incrementQuality()
                 } else {
                     if (!items[i].isBackstage) {
-                        if (!items[i].isSulfuras) {
-                            // normales y conjured
-                            if (items[i].isConjured) {
-                                items[i].quality = items[i].decrementQuality(2)
-                            } else {
-                                items[i].quality = items[i].decrementQuality()
-                            }
+                        // normales y conjured
+                        if (items[i].isConjured) {
+                            items[i].quality = items[i].decrementQuality(2)
+                        } else {
+                            items[i].quality = items[i].decrementQuality()
                         }
                     } else {
                         // Solo backstage
