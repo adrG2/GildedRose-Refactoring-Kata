11a41aceea5197657edebf22b25c3a688b76ddb0
adrg2 <adr.garro@gmail.com>
2022-01-23 22:31:02 +0100

    Invert if statement quality max

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,32 +5,25 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (!items[i].isAgedBrie && !items[i].isBackstage) {
+            if (items[i].isAgedBrie || items[i].isBackstage) {
+                items[i].quality = items[i].incrementQuality()
+
+                if (items[i].isBackstage) {
+                    if (items[i].expireInTenDaysOrLess()) {
+                        items[i].quality = items[i].incrementQuality()
+                    }
+
+                    if (items[i].expireInFiveDaysOrLess()) {
+                        items[i].quality = items[i].incrementQuality()
+                    }
+                }
+            } else {
                 if (items[i].quality > QUALITY_MIN) {
                     if (!items[i].isSulfuras) {
                         if (items[i].isConjured) {
                             items[i].quality = items[i].decrementQuality(2)
                         } else {
                             items[i].quality = items[i].decrementQuality()
-                        }
-                    }
-                }
-            } else {
-                // AgedBrie y Backstage
-                if (items[i].quality < QUALITY_MAX) {
-                    items[i].quality = items[i].incrementQuality()
-
-                    if (items[i].isBackstage) {
-                        if (items[i].expireInTenDaysOrLess()) {
-                            if (items[i].quality < QUALITY_MAX) {
-                                items[i].quality = items[i].incrementQuality()
-                            }
-                        }
-
-                        if (items[i].expireInFiveDaysOrLess()) {
-                            if (items[i].quality < QUALITY_MAX) {
-                                items[i].quality = items[i].incrementQuality()
-                            }
                         }
                     }
                 }
