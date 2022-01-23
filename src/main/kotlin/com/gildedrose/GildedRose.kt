36b15e9e747d55d1885c7e1ed744ddc48c6cff0d
adrg2 <adr.garro@gmail.com>
2022-01-23 23:12:46 +0100

    Extracted item var

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,50 +5,38 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].isSulfuras) continue
-            if (items[i].isAgedBrie || items[i].isBackstage) {
-                items[i].quality = items[i].incrementQuality()
+            val item = items[i]
+            if (item.isSulfuras) continue
+            item.sellIn = item.decrementSellIn()
+            if (item.isAgedBrie || item.isBackstage) {
+                item.quality = item.incrementQuality()
 
-                if (items[i].isBackstage) {
-                    if (items[i].expireInTenDaysOrLess()) {
-                        items[i].quality = items[i].incrementQuality()
+                if (item.isBackstage) {
+                    if (item.expireInTenDaysOrLess()) {
+                        item.quality = item.incrementQuality()
                     }
 
-                    if (items[i].expireInFiveDaysOrLess()) {
-                        items[i].quality = items[i].incrementQuality()
+                    if (item.expireInFiveDaysOrLess()) {
+                        item.quality = item.incrementQuality()
                     }
                 }
             } else {
-                if (items[i].isConjured) {
-                    items[i].quality = items[i].decrementQuality(2)
+                if (item.isConjured) {
+                    item.quality = item.decrementQuality(2)
                 } else {
-                    items[i].quality = items[i].decrementQuality()
+                    item.quality = item.decrementQuality()
                 }
             }
 
-            // normales y conjured
-            items[i].sellIn = items[i].decrementSellIn()
-
-
-            if (items[i].isSellInExpired()) {
-                if (items[i].isAgedBrie) {
-                    items[i].quality = items[i].incrementQuality()
-                } else {
-                    if (!items[i].isBackstage) {
-                        // normales y conjured
-                        if (items[i].isConjured) {
-                            items[i].quality = items[i].decrementQuality(2)
-                        } else {
-                            items[i].quality = items[i].decrementQuality()
-                        }
-                    } else {
-                        // Solo backstage
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
+            if (item.isSellInExpired()) {
+                when {
+                    item.isAgedBrie -> item.quality = item.incrementQuality()
+                    item.isBackstage -> item.quality = 0
+                    item.isConjured -> item.quality = item.decrementQuality(2)
+                    else -> item.quality = item.decrementQuality()
                 }
             }
         }
     }
-
 }
 
